8c6f319b3415e0a9821fea189d2dac09754c742e
yangwenbo <yangwenbo@qq.com>
2019-07-31 15:33:04 +0800

    初始化导入

---
+++ src/main/java/DataTypeDemo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by lenovo on 2019/7/30.
+ * @author yagnwen-bo
+ *
+ * 数据类型练习
+ */
+public class DataTypeDemo {
+    //输入
+    Scanner input=new Scanner(System.in);
+    int a=input.nextInt(); //输入整型
+}
