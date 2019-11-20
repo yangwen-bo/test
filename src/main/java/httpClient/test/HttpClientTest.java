8e48f38fceeca53346554eba1a70c80861d5bbb5
yangwen-bo <1534458078@qq.com>
2019-11-20 10:44:49 +0800

    httpClient 接口

---
+++ src/main/java/httpClient/test/HttpClientTest.java
@@ -0,0 +1,20 @@
+package httpClient.test;
+
+import httpClient.SendHttpClient;
+import httpClient.entity.RequestHttpEntity;
+
+/**
+ * @Author yangwen-bo
+ * @Date 2019/11/6.
+ * @Version 1.0
+ *
+ * 测试httpClient
+ */
+public class HttpClientTest {
+    public static void main(String[] args) {
+
+        RequestHttpEntity requestHttpEntity = new RequestHttpEntity("1355551234","11111","liushuihao","laiyuan");
+
+        SendHttpClient.sendHttp( "http://www.baidu.com",requestHttpEntity );
+    }
+}
